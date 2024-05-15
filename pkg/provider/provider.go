e00daf342b49e874a26bf4ebdd97efcb20e6d513
Toma Puljak <toma.puljak@hotmail.com>
2024-05-15 12:50:51 +0000

    feat: update Daytona to v0.15.0
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -10,7 +10,8 @@ import (
 
 	"github.com/daytonaio/daytona/pkg/logger"
 	"github.com/daytonaio/daytona/pkg/provider"
-	"github.com/daytonaio/daytona/pkg/types"
+	"github.com/daytonaio/daytona/pkg/provider/util"
+	"github.com/daytonaio/daytona/pkg/workspace"
 )
 
 type SampleProvider struct {
@@ -20,10 +21,11 @@ type SampleProvider struct {
 	ServerUrl         *string
 	ServerApiUrl      *string
 	LogsDir           *string
+	NetworkKey        *string
 	OwnProperty       string
 }
 
-func (p *SampleProvider) Initialize(req provider.InitializeProviderRequest) (*types.Empty, error) {
+func (p *SampleProvider) Initialize(req provider.InitializeProviderRequest) (*util.Empty, error) {
 	p.OwnProperty = "my-own-property"
 
 	p.BasePath = &req.BasePath
@@ -32,8 +34,9 @@ func (p *SampleProvider) Initialize(req provider.InitializeProviderRequest) (*ty
 	p.ServerUrl = &req.ServerUrl
 	p.ServerApiUrl = &req.ServerApiUrl
 	p.LogsDir = &req.LogsDir
+	p.NetworkKey = &req.NetworkKey
 
-	return new(types.Empty), nil
+	return new(util.Empty), nil
 }
 
 func (p SampleProvider) GetInfo() (provider.ProviderInfo, error) {
@@ -57,49 +60,50 @@ func (p SampleProvider) GetDefaultTargets() (*[]provider.ProviderTarget, error) 
 		{
 			Name:         "default-target",
 			ProviderInfo: info,
-			Options:      "{\n\t\"Required STring\": \"default-required-string\"\n}",
+			Options:      "{\n\t\"Required String\": \"default-required-string\"\n}",
 		},
 	}
 	return &defaultTargets, nil
 }
 
-func (p SampleProvider) CreateWorkspace(workspaceReq *provider.WorkspaceRequest) (*types.Empty, error) {
+func (p SampleProvider) CreateWorkspace(workspaceReq *provider.WorkspaceRequest) (*util.Empty, error) {
 	logWriter := io.MultiWriter(&log_writers.InfoLogWriter{})
 	if p.LogsDir != nil {
-		wsLogWriter := logger.GetWorkspaceLogger(*p.LogsDir, workspaceReq.Workspace.Id)
+		loggerFactory := logger.NewLoggerFactory(*p.LogsDir)
+		wsLogWriter := loggerFactory.CreateWorkspaceLogger(workspaceReq.Workspace.Id)
 		logWriter = io.MultiWriter(&log_writers.InfoLogWriter{}, wsLogWriter)
 		defer wsLogWriter.Close()
 	}
 
 	logWriter.Write([]byte("Workspace created\n"))
 
-	return new(types.Empty), nil
+	return new(util.Empty), nil
 }
 
-func (p SampleProvider) StartWorkspace(workspaceReq *provider.WorkspaceRequest) (*types.Empty, error) {
-	return new(types.Empty), nil
+func (p SampleProvider) StartWorkspace(workspaceReq *provider.WorkspaceRequest) (*util.Empty, error) {
+	return new(util.Empty), nil
 }
 
-func (p SampleProvider) StopWorkspace(workspaceReq *provider.WorkspaceRequest) (*types.Empty, error) {
-	return new(types.Empty), nil
+func (p SampleProvider) StopWorkspace(workspaceReq *provider.WorkspaceRequest) (*util.Empty, error) {
+	return new(util.Empty), nil
 }
 
-func (p SampleProvider) DestroyWorkspace(workspaceReq *provider.WorkspaceRequest) (*types.Empty, error) {
-	return new(types.Empty), nil
+func (p SampleProvider) DestroyWorkspace(workspaceReq *provider.WorkspaceRequest) (*util.Empty, error) {
+	return new(util.Empty), nil
 }
 
-func (p SampleProvider) GetWorkspaceInfo(workspaceReq *provider.WorkspaceRequest) (*types.WorkspaceInfo, error) {
+func (p SampleProvider) GetWorkspaceInfo(workspaceReq *provider.WorkspaceRequest) (*workspace.WorkspaceInfo, error) {
 	providerMetadata, err := p.getWorkspaceMetadata(workspaceReq)
 	if err != nil {
 		return nil, err
 	}
 
-	workspaceInfo := &types.WorkspaceInfo{
+	workspaceInfo := &workspace.WorkspaceInfo{
 		Name:             workspaceReq.Workspace.Name,
 		ProviderMetadata: providerMetadata,
 	}
 
-	projectInfos := []*types.ProjectInfo{}
+	projectInfos := []*workspace.ProjectInfo{}
 	for _, project := range workspaceReq.Workspace.Projects {
 		projectInfo, err := p.GetProjectInfo(&provider.ProjectRequest{
 			TargetOptions: workspaceReq.TargetOptions,
@@ -115,34 +119,33 @@ func (p SampleProvider) GetWorkspaceInfo(workspaceReq *provider.WorkspaceRequest
 	return workspaceInfo, nil
 }
 
-func (p SampleProvider) CreateProject(projectReq *provider.ProjectRequest) (*types.Empty, error) {
+func (p SampleProvider) CreateProject(projectReq *provider.ProjectRequest) (*util.Empty, error) {
 	logWriter := io.MultiWriter(&log_writers.InfoLogWriter{})
 	if p.LogsDir != nil {
-		wsLogWriter := logger.GetWorkspaceLogger(*p.LogsDir, projectReq.Project.WorkspaceId)
-		projectLogWriter := logger.GetProjectLogger(*p.LogsDir, projectReq.Project.WorkspaceId, projectReq.Project.Name)
-		logWriter = io.MultiWriter(&log_writers.InfoLogWriter{}, wsLogWriter, projectLogWriter)
-		defer wsLogWriter.Close()
+		loggerFactory := logger.NewLoggerFactory(*p.LogsDir)
+		projectLogWriter := loggerFactory.CreateProjectLogger(projectReq.Project.WorkspaceId, projectReq.Project.Name)
+		logWriter = io.MultiWriter(&log_writers.InfoLogWriter{}, projectLogWriter)
 		defer projectLogWriter.Close()
 	}
 
 	logWriter.Write([]byte("Project created\n"))
 
-	return new(types.Empty), nil
+	return new(util.Empty), nil
 }
 
-func (p SampleProvider) StartProject(projectReq *provider.ProjectRequest) (*types.Empty, error) {
-	return new(types.Empty), nil
+func (p SampleProvider) StartProject(projectReq *provider.ProjectRequest) (*util.Empty, error) {
+	return new(util.Empty), nil
 }
 
-func (p SampleProvider) StopProject(projectReq *provider.ProjectRequest) (*types.Empty, error) {
-	return new(types.Empty), nil
+func (p SampleProvider) StopProject(projectReq *provider.ProjectRequest) (*util.Empty, error) {
+	return new(util.Empty), nil
 }
 
-func (p SampleProvider) DestroyProject(projectReq *provider.ProjectRequest) (*types.Empty, error) {
-	return new(types.Empty), nil
+func (p SampleProvider) DestroyProject(projectReq *provider.ProjectRequest) (*util.Empty, error) {
+	return new(util.Empty), nil
 }
 
-func (p SampleProvider) GetProjectInfo(projectReq *provider.ProjectRequest) (*types.ProjectInfo, error) {
+func (p SampleProvider) GetProjectInfo(projectReq *provider.ProjectRequest) (*workspace.ProjectInfo, error) {
 	providerMetadata := provider_types.ProjectMetadata{
 		Property: projectReq.Project.Name,
 	}
@@ -152,12 +155,10 @@ func (p SampleProvider) GetProjectInfo(projectReq *provider.ProjectRequest) (*ty
 		return nil, err
 	}
 
-	projectInfo := &types.ProjectInfo{
+	projectInfo := &workspace.ProjectInfo{
 		Name:             projectReq.Project.Name,
 		IsRunning:        true,
 		Created:          "Created at ...",
-		Started:          "Started at ...",
-		Finished:         "Finished at ...",
 		ProviderMetadata: string(metadataString),
 	}
 
